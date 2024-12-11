c9f84381cd0b0443d750dec662718d98cc3dda9c
Saad <saad_eew@hotmail.com>
2024-12-12 00:27:52 +0500

    Almost complete relatedproduct

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import {useParams} from "react-router-dom";
 import {useContext, useEffect, useState} from "react";
 import {ShopContext} from "../context/ShopContext.jsx";
 import {assets} from "../assets/assets.js";
+import RelatedProducts from "../components/RelatedProducts.jsx";
 
 const Product = () => {
 
@@ -10,6 +11,7 @@ const Product = () => {
     const {products, currency} = useContext(ShopContext);
     const [productData, setProductData] = useState(false);
     const [image, setImage] = useState('');
+    const [size, setSize] = useState('');
 
     const fetchProductData = async () => {
         products.map((item) => {
@@ -32,9 +34,11 @@ const Product = () => {
         <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
             {/* -------------------Product Images--------------- */}
             <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
-                <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full images-row'>
+                <div
+                    className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full images-row'>
                     {productData.image.map((item, index) => (
-                        <img onClick={()=>setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt=""/>
+                        <img onClick={() => setImage(item)} src={item} key={index}
+                             className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt=""/>
                     ))}
                 </div>
                 <div className='w-full sm:w-[80%]'>
@@ -53,8 +57,40 @@ const Product = () => {
                 </div>
                 <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
                 <p className='text-gray-500 mt-5 md:w-4/5 '>{productData.description}</p>
+                <div className='flex flex-col gap-4 my-8'>
+                    <p>Select Size</p>
+                    <div className="flex gap-2">
+                        {productData.sizes.map((item, index) => (
+                            <button
+                                className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`}
+                                key={index} onClick={() => setSize(item)}>{item}</button>
+                        ))}
+                    </div>
+                </div>
+                <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+                <hr className='mt-8 sm:w-4/5'/>
+                <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                    <p>100% Original Product</p>
+                    <p>Cash on delivery is available</p>
+                    <p>Easy return and exchange policy within 7 days</p>
+                </div>
             </div>
         </div>
+        {/* ------------Description and Review-------------- */}
+        <div className="mt-20">
+            <div className="flex">
+                <b className='border px-5 py-3 text-sm'>Description</b>
+                <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+            </div>
+            <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto aut, dicta ducimus in, maiores minus modi molestias odio quaerat quam rerum sit voluptatum. Beatae consectetur cupiditate delectus dolores id molestiae nesciunt obcaecati odio, quod ratione, rerum totam ullam. Delectus illum non rerum temporibus totam. A beatae saepe similique unde.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis harum magnam voluptas? Commodi cumque, debitis dicta dolor ex excepturi exercitationem expedita fuga iure laudantium odit omnis pariatur sapiente sint temporibus ut velit. Accusantium cum dignissimos ea eaque fugit omnis quos. Consectetur consequuntur corporis cum ducimus error esse eveniet iusto modi nobis tempore. Ex nam pariatur unde voluptatem? Harum, quam rem? Alias dolore dolorem ducimus et minima nemo, praesentium repudiandae voluptas.</p>
+            </div>
+        </div>
+
+        {/* ----------Display Related Products---------------- */}
+        <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
+
     </div>) : <div className='opacity-0'></div>
 }
 

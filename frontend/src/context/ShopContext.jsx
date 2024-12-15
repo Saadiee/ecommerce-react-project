9d4eb2639243aa7d39ce31efad980b9f497b5141
Saad <saad_eew@hotmail.com>
2024-12-15 20:27:50 +0500

    Add AddToCart functionality

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,17 +1,34 @@
-import { products } from "../assets/assets";
+import {products} from "../assets/assets";
 import {createContext, useState} from "react";
 
 export const ShopContext = createContext();
 
-const ShopContextProvider = (props) =>{
+const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+        let cartData = structuredClone(cartItems)
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            }
+            else{
+                cartData[itemId][size] = 1;
+            }
+        }else{
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
 
     const value = {
-        products, currency,delivery_fee, search, setSearch, showSearch, setShowSearch,
+        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch,cartItems,addToCart
     }
     return (
         <ShopContext.Provider value={value}>
